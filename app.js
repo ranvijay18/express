a7b05e164267c16b7e51a8960789447f5a99a549
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2023-12-21 14:12:08 +0530

    Update app.js handle the request by using middleware

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
 
 
-app.use((req,res,next) => {
-    console.log("In the Middleware!");
-    next();
+app.use("/add-product", (req,res,next) => {
+    res.send("<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='size'><button type='submit'>Add</button></form>");
+});
+app.post("/product", (req,res,next) => {
+    console.log(req.body);
+    res.redirect('/');
 });
 
-app.use((req,res,next) => {
-    console.log("In another Middleware!");
+app.use("/", (req,res,next) => {
     res.send('<h1>Hello Express.js</h1>')
 });
 
-// const server = http.createServer(routes);
 const server = http.createServer(app);
 
 server.listen(4000);
