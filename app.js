6722d3954bf3ee65758190e8d2d2b04dac357520
Ranvijay Singh <109961637+ranvijay18@users.noreply.github.com>
2023-12-21 15:18:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-const http = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
+const adminRouter = require('./router/admin');
+const shopRouter = require('./router/shop');
+
+
 app.use(bodyParser.urlencoded({extended: false}))
 
+app.use("/admin",adminRouter);
+app.use("/shop",shopRouter);
 
-app.use("/add-product", (req,res,next) => {
-    res.send("<form action='/product' method='POST'><input type='text' name='title'><input type='number' name='size'><button type='submit'>Add</button></form>");
-});
-app.post("/product", (req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
-});
+app.use((req,res,next) => {
+  res.status(404).send('<h1>Page not found</h1>')
+})
 
-app.use("/", (req,res,next) => {
-    res.send('<h1>Hello Express.js</h1>')
-});
 
-const server = http.createServer(app);
 
-server.listen(4000);
+app.listen(4000);
